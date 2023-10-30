563cb429d1f9713c77df1fe9745d42a078e17d8e
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-30 10:06:11 -0700

    Practica_1

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Práctica__uni3_jerh.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+    }
+}
